817bf96ff09bbf3758d9c3e4d977f99578f4be93
tssandeep <109350985+tssandeep@users.noreply.github.com>
2022-07-20 21:10:46 +0530

    Update aiml2.java

--- aiml2.java
+++ aiml2.java
@@ -1,7 +1,7 @@
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
  */
-public class AiMl {
+public class aiml2 {
   public static void main(String[] args) {
     System.out.println("Ai, Ml");
   }
